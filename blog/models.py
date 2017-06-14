b0475aebbd34a2569fb69c53519100c471f18a59
yagami <1131228840@qq.com>
2017-06-14 18:00:32 +0800

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    # 文章标题
+    title = models.CharField(max_length=70)
+    # 文章正文
+    body = models.TextField()
+    # 创建时间、最后一次修改时间
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    # 文章摘要
+    excerpt = models.CharField(max_length=200, blank=True)
+
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    # 文章作者，这里 User 是从 django.contrib.auth.models 导入的。
+    # django.contrib.auth 是 Django 内置的应用，专门用于处理网站用户的注册、登录等流程，User 是 Django 为我们已经写好的用户模型。
+    # 这里我们通过 ForeignKey 把文章和 User 关联了起来。
+    author = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.title
