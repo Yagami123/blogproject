55512435c59be2f32c7e20dd58a56208b26c2c95
yagami <1131228840@qq.com>
2017-06-16 17:57:24 +0800

    add blog-website templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    # return HttpResponse("欢迎光临我的博客!")
+    return render(
+        request, 'blog/blog.html'
+    )
